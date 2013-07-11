6b5d00552eb7e5c5745b8fa998d8a25dac2f2d2d
Mark Juniper <markjnpr685@gmail.com>
2013-07-11 17:03:11 +0000

    added web.js

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+app.get('/', function(request, response) { var fs = require('fs'); var buf = new Buffer (256); response.send(buf/.toString('utc 8',fs.readFileSync("index.html")));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
