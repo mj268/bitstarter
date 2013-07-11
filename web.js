6a1863c24caa6402f2d52b4195141dcf7df9b75b
Mark Juniper <markjnpr685@gmail.com>
2013-07-11 18:23:48 +0000

    web

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+// way to do it without a buffer since fs.readFileSync
+// says if you include encoding option ('utf-8') in 
+// this case then this function returns a string
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-  response.send(buf.toString());
+  var content = fs.readFileSync('index.html', 'utf-8');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
