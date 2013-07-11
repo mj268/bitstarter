dd274e20d06a534a497bb75d1bf6eea58a67251d
Mark Juniper <markjnpr685@gmail.com>
2013-07-11 17:17:47 +0000

    commit web.js

--- web.js
+++ web.js
@@ -2,7 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) { var fs = require('fs'); var buf = new Buffer (256); response.send(buf/.toString('utc 8',fs.readFileSync("index.html")));
+app.get('/', function(request, response) {
+var fs = require('fs'); 
+var buf = new Buffer (): 
+ response.send(buffer.toString('utc 8',fs.readFileSync("index.html")))});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
